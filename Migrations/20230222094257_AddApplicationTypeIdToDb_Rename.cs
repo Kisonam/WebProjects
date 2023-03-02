c6897ecdc5deac5c42503014bb46adb42b64d254
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2023-03-02 01:32:49 +0100

    Add files via upload

---
+++ Migrations/20230222094257_AddApplicationTypeIdToDb_Rename.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPPractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationTypeIdToDb_Rename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationTypeId",
+                table: "Product");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApplicationTypeId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
