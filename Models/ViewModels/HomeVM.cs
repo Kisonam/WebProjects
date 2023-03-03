ded02bba512d4d4127f86852f0d2bbb2af163e3d
Nightmare St <107635756+Kisonam@users.noreply.github.com>
2023-03-03 03:55:32 +0100

    Delete Models directory

--- Models/ViewModels/HomeVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ASPPractice.Models.ViewModels
-{
-    public class HomeVM
-    {
-        public IEnumerable<Product> Products { get; set; }
-        public IEnumerable<Category> Catygories { get; set; } 
-    }
-}
